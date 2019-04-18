08a84ab2b77d136938c8972398415cba88472808
Pavel Proyavin <pavel@dorohovdesign.com>
2019-04-18 17:53:58 +0300

    Changelog from 180419

--- src/js/form.js
+++ src/js/form.js
@@ -4,5 +4,13 @@
 
         $('form').parsley()
 
+        $('#file').on('change', function(){
+            var fn = $(this).val();
+            var filename = fn.match(/[^\\/]*$/)[0];
+
+            $('.form__group.is--file label').html(filename)
+            $('.form__group.is--file label').addClass('is--active')
+
+        })
     })
 })(jQuery);
